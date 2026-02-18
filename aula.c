956cc47fd92ebf0d297fae983baf6d8bf03f0f93
BenicioKushano <beniciokushano@gmail.com>
2026-02-18 17:31:20 -0300

    dobra o numero

--- aula.c
+++ aula.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
 printf ("\t\t@\n\t@\t\t@\n\t\t\t\t@\n\t\t\t@\n\t\t@\n");
 
+int *p = malloc(sizeof(int));
 
+scanf("%i", p);
+
+*p *= 2;
+
+printf ("%i", *p);
 
 
 
